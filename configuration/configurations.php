216728a239d97a3f551da0641f0e2ec09be06f2e
rameem2003 <rameem2019@gmail.com>
2023-06-09 10:28:37 +0600

    update db

--- configuration/configurations.php
+++ configuration/configurations.php
@@ -1,5 +1,6 @@
 <?php 
 
-$conn = mysqli_connect("localhost", "root", "", "rol");
+// $conn = mysqli_connect("localhost", "root", "", "rol");
+$conn = mysqli_connect("us-cdbr-east-06.cleardb.net", "b10285fff07673", "9ffcb8a8", "heroku_54ee89fd486e2ba");
 
 ?>
